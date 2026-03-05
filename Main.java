f8f57364f6395fdf79f008949dd481dc17a0f2b1
YvanKamga7 <yvansteve.fotsokamga@gmail.com>
2026-03-05 22:04:10 +0100

    Ajout d'une nouvelle ligne sur la branche

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Mon premier projet sur GitHub est en ligne !");
+        System.out.println("Ceci est une nouvelle fonctionnalité isolée.");
     }
 }
